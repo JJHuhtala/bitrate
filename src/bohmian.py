fixed type errors, added psi calculation, need to add phase next

src/bohmian.py
@@ -69,7 +69,7 @@ class Trajectory():
         """
         x1 = self.pos[0]
         x2 = self.pos[1]
-        psi = 0
+        psi = fp.psi(self.coeffs, x1,x2,self.L, self.n_basis)
         k1 = np.nan_to_num( (fp.basis2d_x1deriv(self.coeffs, x1, x2, self.L, self.n_basis)/psi).imag  )
         k2 = np.nan_to_num( (fp.basis2d_x2deriv(self.coeffs, x1, x2, self.L, self.n_basis)/psi).imag )
         return [self.pos[0] + self.dt * k1, self.pos[1] + self.dt * k2]
@@ -189,6 +189,8 @@ class BohmianSimulation():
             i.compute_trajectory()
             for k in range(i.get_weight()):
                 traj_xs.append(i.get_trajectory())
+            if i%10 == 0:
+                print("Calculated trajectory: ", i)
         
         np.save(self.output,np.array(traj_xs))
 

stats.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load presaved trajectories. 
-a = np.load("testtraj.npy")
+a = np.load("trajs.npy")
 print(len(a[0,:,0]))
 flighttimes = []
 print(np.max(a[:,0,0]))
 
 for i in range(len(a)):
     for j in range(len(a[0,:,0])):
-        if a[i,j,0] > 3.0:
+        if a[i,j,0] > 1.0:
             flighttimes.append(0.001*j)
             break
 
 for i in range(len(a)):
     for j in range(len(a[0,:,0])):
-        if a[i,j,1] > 3.0:
+        if a[i,j,1] > 1.0:
             flighttimes.append(0.001*j)
             break
 