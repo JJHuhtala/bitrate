fixed type errors, normalization is still off

run.py
@@ -28,7 +28,7 @@ V[:,100]  = 100.0
 V[100,:]  = 100.0
 num_basis_funcs = 40
 
-for i in range(Npoints):
+"""for i in range(Npoints):
     for j in range(Npoints):
         psi[i,j] = 0.7*Y(x[i],1.0,1.0,np.pi/2,A0)*Y(x[j],1.0,1.0,np.pi/2,A0) + 0.7*Y(x[i],1.0,1.0,np.pi+np.pi/2,A0)*Y(x[j],1.0,1.0,np.pi+np.pi/2,A0)
 
@@ -36,15 +36,8 @@ def psi_cont(x,y):
     return np.abs(0.7*Y(x,1.0,1.0,np.pi/2,A0)*Y(y,1.0,1.0,np.pi/2,A0) + 0.7*Y(x,1.0,1.0,np.pi+np.pi/2,A0)*Y(y,1.0,1.0,np.pi+np.pi/2,A0))**2
 norm = ts.get_norm(psi,Npoints,dx)
 psi = psi/np.sqrt(norm)
-from scipy.stats import rv_continuous
+from scipy.stats import rv_continuous"""
 
-class entangled_gen(rv_continuous):
-
-    "Gaussian distribution"
-
-    def _pdf(self, x,y):
-
-        return psi_cont(x,y)
 #print(norm)
 
 def integral(fun1,fun2):
@@ -56,7 +49,13 @@ def E(n1,n2):
 
 coeffs = np.zeros((num_basis_funcs,num_basis_funcs),dtype=complex)
 print("Starting coeffs")
+coeffs = np.loadtxt("coeffs_nowall.txt", dtype=complex)
+coeffs = coeffs/(np.sum(np.abs(coeffs)**2))
+for i in range(Npoints):
+    for j in range(Npoints):
+        psi[i,j] = ts.psi(coeffs,x[i],x[j],0.00,L,40)
 
+print(np.sum(psi*dx*dx))
 """for i in range(num_basis_funcs):
     for j in range(num_basis_funcs):
         bs = ts.basis2d(x,L,i,j,Npoints)
@@ -101,8 +100,8 @@ plt.show()"""
 #plt.cla()
 #Animate everything
 
-bb = BohmianSimulation(psi, x, L, 1000, timestep, 10000)
-bb.calculate_trajectories()
+#bb = BohmianSimulation(psi, x, L, 1000, timestep, 100)
+#bb.calculate_trajectories()
 
 
 

src/bohmian.py
@@ -71,7 +71,7 @@ class Trajectory():
         x2 = self.pos[1]
         t = step*self.dt
         psi = fp.psi(self.coeffs, x1,x2,t,self.L, self.n_basis)
-        k1 = np.nan_to_num( (fp.basis2d_x1deriv(self.coeffs, x1, x2, t, self.L, self.n_basis)/psi).imag  )
+        k1 = np.nan_to_num( (fp.basis2d_x1deriv(self.coeffs, x1, x2, t, self.L, self.n_basis)/psi).imag )
         k2 = np.nan_to_num( (fp.basis2d_x2deriv(self.coeffs, x1, x2, t, self.L, self.n_basis)/psi).imag )
         return [self.pos[0] + self.dt * k1, self.pos[1] + self.dt * k2]
     
@@ -84,7 +84,7 @@ class Trajectory():
         as a numpy array the positions of the particle at each timestamp.
         """
         for i in range(self.Nt):
-            self.pos = self.euler_step_cython()
+            self.pos = self.euler_step_cython(i)
             self.traj.append(np.copy(self.pos))
 
     def get_trajectory(self):
@@ -147,6 +147,7 @@ class BohmianSimulation():
         self.Nt = Nt
         self.L = L
         self.coeffs = np.loadtxt(coeff_file, dtype=complex)
+        print(np.sum(np.abs(self.coeffs)**2))
         self.output = output
     def generate_initial_distribution(self):
         """Generates the initial distribution of indices in the psi0 array. Looks a bit confusing, but easily understood
@@ -186,13 +187,14 @@ class BohmianSimulation():
 
         print("Number of different trajectories: ", len(trajectories), ", starting calculation..")
         traj_xs = []
+        counter = 0
         for i in trajectories:
             i.compute_trajectory()
             for k in range(i.get_weight()):
                 traj_xs.append(i.get_trajectory())
-            if i%10 == 0:
-                print("Calculated trajectory: ", i)
-        
+            if counter%10 == 0:
+                print("Calculated trajectory: ", counter)
+            counter += 1
         np.save(self.output,np.array(traj_xs))
 
 if __name__=="__main__":